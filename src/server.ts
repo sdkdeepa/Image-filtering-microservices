8901caad5baf6d6d9fc7484fc3cad648ecf35f94
sdkdeepa <deepa94086@gmail.com>
2020-11-16 08:57:14 -0800

    Commiting to Dev
    Adding all the files in Dev

---
+++ src/server.ts
@@ -0,0 +1,64 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { reduce } from 'bluebird';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  app.get("/filteredImage", async (req, res) => {
+
+    const imageUrl = req.query.imageUrl;
+    if (imageUrl) {
+      filterImageFromURL(imageUrl).then((response) => {
+        res.sendFile(response);
+        res.on("finish", function () {
+          deleteLocalFiles([response])
+        })
+      })
+    } else {
+      res.status(404).send("The Image url is incorrect, Please provide a valid url");
+    }
+  })
+
+
+  //! END @TODO1
+
+  // Root Endpoint
+
+
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
